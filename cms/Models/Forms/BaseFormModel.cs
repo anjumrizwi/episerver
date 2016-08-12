f7e419d68035c44301163f71cbb693e7ffbfb3af
Anjum Rizwi <anjumrizwi@gmail.com>
2016-08-12 19:26:46 +0530

    User Registration module partially created

---
+++ cms/Models/Forms/BaseFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServerSimpleSite.Models.Blocks;
+
+namespace EPiServerSimpleSite.Models.Forms
+{
+    public class BaseFormModel<T> where T : SiteBlockData
+    {
+        public PageReference CurrentPageLink { get; set; }
+
+        public ContentReference CurrentBlockLink { get; set; }
+
+        public string CurrentLanguage { get; set; }
+
+        public T ParentBlock { get; set; }
+    }
+}
