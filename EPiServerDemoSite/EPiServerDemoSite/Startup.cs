ba8d1cd15da30f1947fe94608c9dfd33ccd1667d
Anjum Rizwi <anjumrizwi@gmail.com>
2016-08-29 13:04:33 +0530

    TODO Mixed mode OWIN authentication

--- EPiServerDemoSite/EPiServerDemoSite/Startup.cs
+++ EPiServerDemoSite/EPiServerDemoSite/Startup.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Owin;
-using System;
-using System.Web.Helpers;
+﻿using EPiServerDemoSite.Models.Account;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using EPiServerDemoSite.Models.Account;
+using System.Web.Helpers;
 
 [assembly: OwinStartup(typeof(EPiServerDemoSite.Startup))]
 
@@ -92,6 +95,77 @@ namespace EPiServerDemoSite
             //   appSecret: "");
 
             //app.UseGoogleAuthentication();
+        }
+
+        /// <summary>
+        /// http://world.episerver.com/documentation/Items/Developers-Guide/Episerver-CMS/8/Security/mixed-mode-owin-authentication/
+        /// </summary>
+        /// <param name="app"></param>
+        public void ConfigureAuth_todo(IAppBuilder app)
+        {
+            //This will configure cookie authentication at the following urls.  In those pages you are responsible for authentication and calling the OwinContext.Authentication.SignIn method to properly sign in the user
+            // and  OwinContext.Authentication.SignOut to logout a user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Application",
+                LoginPath = new PathString("/Login"),
+                LogoutPath = new PathString("/Logout")
+            });
+
+            //This will set ADFS as the default authentication provider 
+            app.SetDefaultSignInAsAuthenticationType(Microsoft.Owin.Security.WsFederation.WsFederationAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+           {
+               AuthenticationType = Microsoft.Owin.Security.WsFederation.WsFederationAuthenticationDefaults.AuthenticationType
+           });
+            //Enable federated authentication
+            app.UseWsFederationAuthentication(new Microsoft.Owin.Security.WsFederation.WsFederationAuthenticationOptions()
+            {
+                //Trusted URL to federation server meta data
+                MetadataAddress = "https://devdc.dev.test/federationmetadata/2007-06/federationmetadata.xml",
+                //Value of Wtreal must *exactly* match what is configured in the federation server
+                Wtrealm = "http://localhost:61528/",
+                Notifications = new Microsoft.Owin.Security.WsFederation.WsFederationAuthenticationNotifications()
+                {
+                    RedirectToIdentityProvider = (ctx) =>
+                        {
+                            //To avoid a redirect loop to the federation server send 403 when user is authenticated but does not have access
+                            if (ctx.OwinContext.Response.StatusCode == 401 && ctx.OwinContext.Authentication.User.Identity.IsAuthenticated)
+                            {
+                                ctx.OwinContext.Response.StatusCode = 403;
+                                ctx.HandleResponse();
+                            }
+                            return Task.FromResult(0);
+                        },
+                    SecurityTokenValidated = (ctx) =>
+                        {
+                            //Ignore scheme/host name in redirect Uri to make sure a redirect to HTTPS does not redirect back to HTTP
+                            var redirectUri = new Uri(ctx.AuthenticationTicket.Properties.RedirectUri, UriKind.RelativeOrAbsolute);
+                            if (redirectUri.IsAbsoluteUri)
+                            {
+                                ctx.AuthenticationTicket.Properties.RedirectUri = redirectUri.PathAndQuery;
+                            }
+                            //Sync user and the roles to EPiServer in the background
+                            EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<EPiServer.Security.SynchronizingUserService>().SynchronizeAsync(ctx.AuthenticationTicket.Identity);
+                            return Task.FromResult(0);
+                        }
+                }
+            });
+            //Add stage marker to make sure WsFederation runs on Authenticate (before URL Authorization and virtual roles)
+            app.UseStageMarker(PipelineStage.Authenticate);
+
+            //Remap logout to a federated logout
+            app.Map(LogoutUrl, map =>
+            {
+                map.Run(ctx =>
+                {
+                    ctx.Authentication.SignOut();
+                    return Task.FromResult(0);
+                });
+            });
+
+            //Tell antiforgery to use the name claim
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Name;
         }
     }
 }
